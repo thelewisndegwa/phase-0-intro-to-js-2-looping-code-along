4efd5c71372389fdbfb959a25330b505fbeca823
thelewisndegwa <lewiskanyi77@gmail.com>
2023-03-10 08:49:40 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+function countDown(num) {
+    while(num >= 0) {
+      console.log(num);
+      num--
+    }
+  }
+  countDown(10);
+
+
+  function writeCards()
+
+
+  let names = ["Ada", "Brendan", "Ali"];
+function writeCards(names, birthday) {
+  for (let i = 0; i < names.length; i++) {
+    console.log(`Thank you, ${names[i]}, for the wonderful ${birthday} gift!`);
+  }
+  return names;
+}
+writeCards();
